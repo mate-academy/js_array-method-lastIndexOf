9390bb2e989d7e75daa6a9f010a06e5c859deb2e
Vadim Ilchenko <v.ilchenko@mate.academy>
2019-04-03 14:43:36 +0300

    fixed standard / standart spelling. Fixed eslint errors.

--- src/arrayMethodLastIndexOf.js
+++ src/arrayMethodLastIndexOf.js
@@ -6,8 +6,8 @@
  * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/lastIndexOf
  */
 
-function applyCustomLastIndexOf() {
-  [].__proto__.lastIndexOf2 = function() {
+function applyCustomLastIndexOf () {
+  [].__proto__.lastIndexOf2 = function () {
     // write code here
   };
 }
