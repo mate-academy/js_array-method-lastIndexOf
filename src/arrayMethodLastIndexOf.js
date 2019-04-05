265e54d2fc14acc3cbe5814bf86aebdbb7106739
Vadim Ilchenko <v.ilchenko@mate.academy>
2019-04-05 14:41:10 +0300

    removed empty line after doc, changed typeof checking to .toBeinstanceOf, moved expectation checks to new line, removed package-lock.json from .gitignore

--- src/arrayMethodLastIndexOf.js
+++ src/arrayMethodLastIndexOf.js
@@ -3,9 +3,7 @@
 /**
  * Implement method lastIndexOf
  *
- * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/lastIndexOf
  */
-
 function applyCustomLastIndexOf() {
   [].__proto__.lastIndexOf2 = function() {
     // write code here
