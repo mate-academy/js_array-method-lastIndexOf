21f8d85f1df8fd3a019b00fc41e0f028c12a4376
Vitalii <ilovepinkpony1@gmail.com>
2019-04-11 22:55:53 +0300

    solution

--- src/arrayMethodLastIndexOf.js
+++ src/arrayMethodLastIndexOf.js
@@ -6,22 +6,11 @@
  */
 function applyCustomLastIndexOf() {
   [].__proto__.lastIndexOf2 = function(token, index = 0) {
-    let lastIndex;
-    if (index < 0) {
-      for (let i = index + this.length; i >= 0; i--) {
-        if (this[i] === token) {
-          lastIndex = i;
-          return lastIndex;
-        }
-      }
-      if (lastIndex === undefined) return -1;
+    if (token === undefined) return -1;
+    for (let i = index + this.length; i >= 0; i--) {
+      if (this[i] === token) return i;
     }
-    for (let i = index; i < this.length; i++) {
-      if (this[i] === token) lastIndex = i;
-    }
-    if (lastIndex === undefined) return -1;
-    return lastIndex;
+    return -1;
   };
 }
-
 module.exports = applyCustomLastIndexOf;
