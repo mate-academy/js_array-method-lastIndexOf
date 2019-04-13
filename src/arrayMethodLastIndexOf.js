48ea206bd762c1dcf161e2abf886c702d665afe3
Alexandr <alexandrmail32@gmail.com>
2019-04-13 23:35:59 +0300

    Solution

--- src/arrayMethodLastIndexOf.js
+++ src/arrayMethodLastIndexOf.js
@@ -5,8 +5,23 @@
  *
  */
 function applyCustomLastIndexOf() {
-  [].__proto__.lastIndexOf2 = function() {
-    // write code here
+  [].__proto__.lastIndexOf2 = function(item, position) {
+    let pos;
+    if (position >= this.length || !position) {
+      pos = this.length - 1;
+    }
+    if (position < 0) {
+      pos = this.length + position;
+    }
+    if (pos < 0) {
+      return -1;
+    }
+    for (var i = pos; i >= 0; i--) {
+      if (item === this[i]) {
+        return i;
+      }
+    }
+    return -1;
   };
 }
 
