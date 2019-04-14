79af84663f809a52ed09d81fb28ad25d5ee3397f
Yevhen-Kryvdiuk <krivachacha@gmail.com>
2019-04-14 16:26:25 +0300

    Yevhen Kryvdiuk

--- src/arrayMethodLastIndexOf.js
+++ src/arrayMethodLastIndexOf.js
@@ -5,8 +5,31 @@
  *
  */
 function applyCustomLastIndexOf() {
-  [].__proto__.lastIndexOf2 = function() {
-    // write code here
+  [].__proto__.lastIndexOf2 = function(searchElement, fromIndex = this.length) {
+    if (searchElement === undefined) {
+      return -1;
+    }
+    let index = fromIndex;
+    if (fromIndex < 0) {
+      if (fromIndex < (-this.length)) {
+        return -1;
+      }
+      index = this.length + fromIndex;
+    }
+    for (index; index >= 0; index--) {
+      if (typeof searchElement === typeof this[index]) {
+        if (searchElement === this[index]) {
+          return index;
+        } else if (isNaN(searchElement) && isNaN(this[index])) {
+          return index;
+        } else {
+          continue;
+        }
+      } else {
+        continue;
+      }
+    }
+    return -1;
   };
 }
 
