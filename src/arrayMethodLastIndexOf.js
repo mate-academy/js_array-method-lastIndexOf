2abfe7d728f3aad5dc23ec98a781ab3661c5ed5c
VladimirTi <tytmes@mail.ru>
2019-04-18 19:58:26 +0300

    dont_mutate_args

--- src/arrayMethodLastIndexOf.js
+++ src/arrayMethodLastIndexOf.js
@@ -5,8 +5,9 @@
  *
  */
 function applyCustomLastIndexOf() {
-  [].__proto__.lastIndexOf2 = function(value, fromIndex = this.length) {
+  [].__proto__.lastIndexOf2 = function(value, inputIndex = this.length) {
     if (value === undefined || this.length === 0) return -1;
+    let fromIndex = inputIndex;
     if (fromIndex < 0) fromIndex = this.length + fromIndex;
     for (fromIndex; fromIndex >= 0; fromIndex--) {
       if (this[fromIndex] === value || (isNaN(value) && isNaN(this[fromIndex]))) {
