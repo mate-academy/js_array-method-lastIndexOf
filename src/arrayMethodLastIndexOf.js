173da05e81d5309e634b875c8d784c3c02134330
valeriitovstyk <valeriitovstyk@gmail.com>
2019-04-19 08:41:59 +0300

    cnahges separate if conditions into one chain OR operator

--- src/arrayMethodLastIndexOf.js
+++ src/arrayMethodLastIndexOf.js
@@ -6,9 +6,12 @@
 function applyCustomLastIndexOf() {
   [].__proto__.lastIndexOf2 = function(searchElement, fromIndex = 0) {
     const arrayLength = this.length;
-    if (arrayLength === 0) return -1;
-    if (fromIndex >= this.length) return -1;
-    if (searchElement === undefined) return -1;
+    // if (arrayLength === 0) return -1;
+    // if (fromIndex >= this.length) return -1;
+    // if (searchElement === undefined) return -1;
+    if (arrayLength === 0 || fromIndex >= this.length || searchElement === undefined) {
+      return -1;
+    }
 
     for (let i = fromIndex + this.length; i >= 0; i--) {
       if (searchElement === this[i]) return i;
