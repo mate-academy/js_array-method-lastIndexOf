6f41cb91e74335e6cbb416ea31e7e525284a924d
AnnaKuvarina <anya.kuvarina@gmail.com>
2019-06-20 11:08:39 +0300

    correct start value

--- src/arrayMethodLastIndexOf.js
+++ src/arrayMethodLastIndexOf.js
@@ -5,11 +5,13 @@
  */
 function applyCustomLastIndexOf() {
   [].__proto__.lastIndexOf2 = function(searchElement, fromIndex) {
+    let start = 0;
     if (searchElement === undefined) {
       return -1;
     }
 
-    for (let i = this.length + fromIndex || this.length - 1; i >= 0; i--) {
+    start = (fromIndex < 0) ? -fromIndex : fromIndex;
+    for (let i = this.length - start || this.length - 1; i >= 0; i--) {
       if (this[i] === searchElement) {
         return i;
       }
