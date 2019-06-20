453002848cdbbe22cd23532173cf889d0cc1038b
OleksandrHlybchenko <logista@meta.ua>
2019-06-20 15:19:14 +0300

    :beers: solution

--- src/arrayMethodLastIndexOf.js
+++ src/arrayMethodLastIndexOf.js
@@ -4,8 +4,18 @@
  * Implement method lastIndexOf
  */
 function applyCustomLastIndexOf() {
-  [].__proto__.lastIndexOf2 = function(searchElement, fromIndex) {
-    for (let i = this.length + fromIndex || this.length - 1; i >= 0; i--) {
+  [].__proto__.lastIndexOf2 = function(
+    searchElement,
+    fromIndex = this.length - 1
+  ) {
+    let i;
+    if (fromIndex < 0) {
+      i = this.length + fromIndex;
+    } else {
+      i = fromIndex;
+    }
+
+    for (i; i >= 0; i--) {
       if (this[i] === searchElement) {
         return i;
       }
@@ -14,5 +24,9 @@ function applyCustomLastIndexOf() {
     return -1;
   };
 }
+
+applyCustomLastIndexOf();
+console.log([1, 2, 3, 1, 2, 3].lastIndexOf(2, 1));
+console.log([1, 2, 3, 1, 2, 3].lastIndexOf2(2, 1));
 
 module.exports = applyCustomLastIndexOf;
