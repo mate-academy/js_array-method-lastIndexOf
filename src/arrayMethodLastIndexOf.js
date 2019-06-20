282eacc4d209b901b15a2449d26dd317844400f9
h2ash <hun22ash@gmail.com>
2019-06-21 02:43:43 +0300

    Solution

--- src/arrayMethodLastIndexOf.js
+++ src/arrayMethodLastIndexOf.js
@@ -4,9 +4,33 @@
  * Implement method lastIndexOf
  */
 function applyCustomLastIndexOf() {
-  [].__proto__.lastIndexOf2 = function(searchElement, fromIndex) {
+  [].__proto__.lastIndexOf2
+  = function(searchElement, fromIndex = (this.length - 1)) {
     // write code here
+    let newIndex = fromIndex;
+
+    if (fromIndex < 0) {
+      newIndex = (this.length + fromIndex);
+    }
+
+    for (let i = newIndex; i > -1; i--) {
+      if (searchElement === this[i]) {
+        return i;
+      }
+    }
+
+    return -1;
   };
 }
+
+/*
+1. считать в обратную сторону цикл
+  1.1 let i = this.length - fromIndex; //Дополнительно указанная позиция
+  1.2 i > -1;
+  1.3. i--; //обратная/отрицательная итерация
+
+добавить проверку на положительный fromIndex
+  если да, то сместить в другую сторону
+*/
 
 module.exports = applyCustomLastIndexOf;
