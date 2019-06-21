d943a05461e656063675de66154dd6abf646184c
Yevgeniy <zleonenkov@gmail.com>
2019-06-21 11:17:24 +0300

    Update

--- src/arrayMethodLastIndexOf.js
+++ src/arrayMethodLastIndexOf.js
@@ -5,7 +5,10 @@
  */
 function applyCustomLastIndexOf() {
   [].__proto__.lastIndexOf2 = function(searchElement, fromIndex) {
-    for (let i = this.length + fromIndex || this.length - 1; i >= 0; i--) {
+    const len = this.length + fromIndex;
+    const lastObj = this.length - 1;
+
+    for (let i = len || lastObj; i >= 0; i--) {
       if (this[i] === searchElement) {
         return i;
       }
