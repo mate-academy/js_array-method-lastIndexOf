c8b76f5cfa61ae489fc683aee7be36ac7707a764
GitHubForDmitry <dmitrykoka@gmail.com>
2019-06-22 07:05:48 +0300

    sol

--- src/arrayMethodLastIndexOf.js
+++ src/arrayMethodLastIndexOf.js
@@ -5,8 +5,17 @@
  */
 function applyCustomLastIndexOf() {
   [].__proto__.lastIndexOf2 = function(searchElement, fromIndex) {
-    // write code here
+    let newfromIndex = fromIndex;
+    newfromIndex = this.length - 1;
+    if (fromIndex < 0) {
+      newfromIndex = (this.length + fromIndex);
+    }
+    for (let i = newfromIndex; i >= 0; i--) {
+      if (this[i] === searchElement) {
+        return i;
+      }
+    }
+    return -1;
   };
 }
-
 module.exports = applyCustomLastIndexOf;
