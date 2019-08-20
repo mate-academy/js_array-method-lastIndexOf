ec5e8ccf7439f94aa5ccb84e27ace5dd356e04c2
yevhen-sahach <antitop04@gmail.com>
2019-08-21 00:59:07 +0300

    first commit

--- src/arrayMethodLastIndexOf.js
+++ src/arrayMethodLastIndexOf.js
@@ -5,7 +5,23 @@
  */
 function applyCustomLastIndexOf() {
   [].__proto__.lastIndexOf2 = function(searchElement, fromIndex) {
-    // write code here
+    let i;
+
+    if (typeof fromIndex === 'number') {
+      fromIndex < 0
+        ? i = fromIndex + this.length
+        : i = fromIndex;
+    } else {
+      i = this.length - 1;
+    }
+
+    for (i; i >= 0; i--) {
+      if (this[i] === searchElement) {
+        return i;
+      }
+    }
+
+    return -1;
   };
 }
 
