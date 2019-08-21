c355330d95b3641233840b06fd923f7d27d69b37
Viktor Pastushenko <pastushenko.vik@gmail.com>
2019-08-21 06:27:21 +0300

    Solution

--- src/arrayMethodLastIndexOf.js
+++ src/arrayMethodLastIndexOf.js
@@ -3,9 +3,28 @@
 /**
  * Implement method lastIndexOf
  */
+const indexNormalisation = (index, length) => {
+  const ind = Math.abs(index) > length - 1
+    ? Math.sign(index) * length
+    : index;
+
+  return ind < 0 ? length + ind : ind;
+};
+
 function applyCustomLastIndexOf() {
   [].__proto__.lastIndexOf2 = function(searchElement, fromIndex) {
-    // write code here
+    let i = fromIndex === undefined
+      ? this.length - 1
+      : indexNormalisation(fromIndex, this.length);
+
+    while (i >= 0) {
+      if (this[i] === searchElement) {
+        return i;
+      }
+      i--;
+    }
+
+    return -1;
   };
 }
 
