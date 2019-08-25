ad480723ee4fc2c3f94c49f1fe95e84239e49653
unknown <shklyarigor2015@gmail.com>
2019-08-26 01:14:25 +0300

    solution

--- src/arrayMethodLastIndexOf.js
+++ src/arrayMethodLastIndexOf.js
@@ -4,8 +4,16 @@
  * Implement method lastIndexOf
  */
 function applyCustomLastIndexOf() {
-  [].__proto__.lastIndexOf2 = function(searchElement, fromIndex) {
-    // write code here
+  [].__proto__.lastIndexOf2 = function(searchElement, fromIndex = 0) {
+    let index = fromIndex >= 0 ? this.length - 1 : this.length + fromIndex;
+
+    for (; index > fromIndex; index--) {
+      if (searchElement === this[index]) {
+        return index;
+      }
+    }
+
+    return -1;
   };
 }
 
