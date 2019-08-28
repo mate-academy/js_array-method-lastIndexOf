2ee9221059675ac6f4f75826a7d77c6f84a7043d
Pukhnichenko <victorpuhnichenko@gmail.com>
2019-08-28 12:35:23 +0300

    'Fixed-v.2'

--- src/arrayMethodLastIndexOf.js
+++ src/arrayMethodLastIndexOf.js
@@ -4,13 +4,15 @@
  * Implement method lastIndexOf
  */
 function applyCustomLastIndexOf() {
-  [].__proto__.lastIndexOf2 = function(searchElement,
-    fromIndex = this.length - 1) {
-    let firstIndex = fromIndex;
+  [].__proto__.lastIndexOf2 = function(
+    searchElement,
+    fromIndex = this.length - 1
+  ) {
+    let startIndex = fromIndex;
     if (fromIndex < 0) {
-      firstIndex = this.length + fromIndex;
+      startIndex = this.length + fromIndex;
     }
-    for (let i = firstIndex; i >= 0; i--) {
+    for (let i = startIndex; i >= 0; i--) {
       if (this[i] === searchElement) {
         return i;
       };
