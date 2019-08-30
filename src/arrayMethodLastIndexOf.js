70b6ad161d59a80c2648393ca6a041ff80ad9b4f
Illia Bezkorovainyi <illia.bezkorovainyi@heathmont.net>
2019-08-30 12:30:32 +0300

    refactoring

--- src/arrayMethodLastIndexOf.js
+++ src/arrayMethodLastIndexOf.js
@@ -4,13 +4,13 @@
  * Implement method lastIndexOf
  */
 function applyCustomLastIndexOf() {
-  [].__proto__.lastIndexOf2 = function(searchElement, fromIndex) {
+  [].__proto__.lastIndexOf2 = function(searchElement, fromIndex = this.length) {
     if (this.length === 0 || searchElement === undefined) {
       return -1;
     }
 
     let i;
-    fromIndex >= this.length || fromIndex === undefined ? i = this.length
+    fromIndex >= this.length ? i = this.length
       : fromIndex < 0 ? i = this.length + fromIndex : i = fromIndex;
 
     for (i - 1; i >= 0; i--) {
