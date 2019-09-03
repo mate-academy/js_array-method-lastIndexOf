ce0b632714ce9870360fca11cc453a36f118fe6f
unknown <shklyarigor2015@gmail.com>
2019-09-03 15:29:27 +0300

    fix

--- src/arrayMethodLastIndexOf.js
+++ src/arrayMethodLastIndexOf.js
@@ -3,16 +3,17 @@
 /**
  * Implement method lastIndexOf
  */
-function applyCustomLastIndexOf() {
-  [].__proto__.lastIndexOf2 = function(searchElement, fromIndex = 0) {
-    let index = fromIndex >= 0 ? this.length - 1 : this.length + fromIndex;
 
-    for (; index > fromIndex; index--) {
-      if (searchElement === this[index]) {
-        return index;
+function applyCustomLastIndexOf() {
+  [].__proto__.lastIndexOf2 = function(searchElement,
+    fromIndex = this.length - 1) {
+    let i = fromIndex < 0 ? this.length + fromIndex : fromIndex;
+
+    for (i; i >= 0; i--) {
+      if (searchElement === this[i]) {
+        return i;
       }
     }
-
     return -1;
   };
 }
