fca48e39ec621c1c97e5146253bfe01a4d9f6a5d
Serhii <gespenst.serge@yandex.ru>
2019-10-02 18:25:12 +0300

    sol

--- src/arrayMethodLastIndexOf.js
+++ src/arrayMethodLastIndexOf.js
@@ -5,7 +5,21 @@
  */
 function applyCustomLastIndexOf() {
   [].__proto__.lastIndexOf2 = function(searchElement, fromIndex) {
-    // write code here
+    if (searchElement === undefined) {
+      return -1;
+    }
+    let startEnd = fromIndex;
+    if (!startEnd) {
+      startEnd = this.length;
+    } else if (startEnd < 0) {
+      startEnd = this.length - Math.abs(startEnd);
+    }
+    for (let i = startEnd; i >= 0; i--) {
+      if (searchElement === this[i]) {
+        return i;
+      }
+    }
+    return -1;
   };
 }
 
