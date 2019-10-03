d66ec3750a2f14b82088fff5bbd9e34d76e8580b
VadimK <djvadya16@gmail.com>
2019-10-03 14:25:03 +0300

    done

--- src/arrayMethodLastIndexOf.js
+++ src/arrayMethodLastIndexOf.js
@@ -5,7 +5,31 @@
  */
 function applyCustomLastIndexOf() {
   [].__proto__.lastIndexOf2 = function(searchElement, fromIndex) {
-    // write code here
+    if (searchElement === undefined) {
+      return -1;
+    };
+
+    if (fromIndex === undefined) {
+      for (let i = this.length; i >= 0; i--) {
+        if (this[i] === searchElement) {
+          return i;
+        }
+      }
+    } else if (fromIndex < 0) {
+      for (let i = this.length + fromIndex; i >= 0; i--) {
+        if (this[i] === searchElement) {
+          return i;
+        }
+      }
+    } else {
+      for (let i = fromIndex; i >= 0; i--) {
+        if (this[i] === searchElement) {
+          return i;
+        }
+      }
+    }
+
+    return -1;
   };
 }
 
