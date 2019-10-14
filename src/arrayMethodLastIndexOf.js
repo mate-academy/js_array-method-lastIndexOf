0c029614c0392ae31b014fca59c243a2fcf8834e
Leon <FeelineKing@gmail.com>
2019-10-15 02:26:55 +0300

    Solution

--- src/arrayMethodLastIndexOf.js
+++ src/arrayMethodLastIndexOf.js
@@ -5,7 +5,41 @@
  */
 function applyCustomLastIndexOf() {
   [].__proto__.lastIndexOf2 = function(searchElement, fromIndex) {
-    // write code here
+    let startLength = this.length - 1;
+
+    if (arguments.length === 0) {
+      return -1;
+    }
+
+    if (fromIndex) {
+      if (fromIndex >= 0) {
+        startLength = fromIndex;
+      } else {
+        startLength = this.length + fromIndex;
+      }
+
+      if (startLength > this.length) {
+        startLength = this.length - 1;
+      }
+
+      if (startLength < 0) {
+        return -1;
+      }
+    }
+
+    for (let i = startLength; i >= 0; i--) {
+      if (searchElement === isNaN) {
+        if (this[i] === isNaN) {
+          return i;
+        };
+      };
+
+      if (this[i] === searchElement) {
+        return i;
+      };
+    }
+
+    return -1;
   };
 }
 
