8766dfd0de97c46b066e7971d38ea1eccf1eca89
Denys <denys.cheporniuk@gmail.com>
2019-11-19 17:29:00 +0200

    fix lastIndex

--- src/arrayMethodLastIndexOf.js
+++ src/arrayMethodLastIndexOf.js
@@ -4,17 +4,21 @@
  * Implement method lastIndexOf
  */
 function applyCustomLastIndexOf() {
-  [].__proto__.lastIndexOf2 = function(searchElement, fromIndex = 0) {
+  [].__proto__.lastIndexOf2 = function(searchElement, fromIndex = this.length) {
     if (searchElement === undefined) {
       return -1;
-    };
+    }
 
-    for (let i = this.length + fromIndex; i >= 0; i--) {
+    let index = fromIndex;
+    if (fromIndex < 0) {
+      index = this.length + fromIndex;
+    }
+
+    for (let i = index; i >= 0; i--) {
       if (this[i] === searchElement) {
         return i;
       }
     }
-
     return -1;
   };
 }
