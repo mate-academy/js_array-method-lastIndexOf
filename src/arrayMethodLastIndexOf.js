56ec4ae19692b1372365bfd2bc9b6a79647d0210
Alexii Aidinov <alexii.aidinov@gmail.com>
2019-11-19 23:49:28 +0200

    Aidinov Alexii

--- src/arrayMethodLastIndexOf.js
+++ src/arrayMethodLastIndexOf.js
@@ -4,8 +4,37 @@
  * Implement method lastIndexOf
  */
 function applyCustomLastIndexOf() {
-  [].__proto__.lastIndexOf2 = function(searchElement, fromIndex) {
-    // write code here
+  [].__proto__.lastIndexOf2 = function(
+    searchElement,
+    fromIndex = this.length - 1
+  ) {
+    if (this.length === 0 || arguments.length === 0) {
+      return -1;
+    }
+
+    let start;
+
+    if (fromIndex < 0) {
+      start = this.length + fromIndex;
+    } else {
+      start = fromIndex;
+    }
+
+    if (fromIndex >= this.length) {
+      return -1;
+    }
+
+    for (let i = start; i >= 0; i--) {
+      if (this[i] === searchElement) {
+        return i;
+      }
+
+      if (isNaN(this[i]) && isNaN(searchElement) && this[i] !== undefined) {
+        return i;
+      }
+    }
+
+    return -1;
   };
 }
 
