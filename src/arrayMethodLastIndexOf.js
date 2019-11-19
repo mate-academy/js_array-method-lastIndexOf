e3738629e1e9d157b7a16991af79a493ecbd95d1
Anna Gerdii <annagerdiy@gmail.com>
2019-11-19 14:39:08 +0200

    solution

--- src/arrayMethodLastIndexOf.js
+++ src/arrayMethodLastIndexOf.js
@@ -3,9 +3,32 @@
 /**
  * Implement method lastIndexOf
  */
+
 function applyCustomLastIndexOf() {
-  [].__proto__.lastIndexOf2 = function(searchElement, fromIndex) {
-    // write code here
+  [].__proto__.lastIndexOf2 = function(searchElement, fromIndex = 0) {
+    if (searchElement === undefined) {
+      return -1;
+    }
+
+    if (fromIndex >= 0) {
+      for (let i = this.length; i > fromIndex; i--) {
+        if (searchElement === this[i]
+          || (isNaN(searchElement)
+            && isNaN(searchElement) === isNaN(this[i]))) {
+          return i;
+        }
+      }
+    } else {
+      for (let i = this.length + fromIndex; i >= 0; i--) {
+        if (searchElement === this[i]
+          || (isNaN(searchElement)
+            && isNaN(searchElement) === isNaN(this[i]))) {
+          return i;
+        }
+      }
+    }
+
+    return -1;
   };
 }
 
