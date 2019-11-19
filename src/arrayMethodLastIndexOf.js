dfed05c9982d61587e95c971acaa21d6c254ae17
helmax-y <max_rad@ukr.net>
2019-11-19 22:57:50 +0200

    Solution

--- src/arrayMethodLastIndexOf.js
+++ src/arrayMethodLastIndexOf.js
@@ -5,7 +5,26 @@
  */
 function applyCustomLastIndexOf() {
   [].__proto__.lastIndexOf2 = function(searchElement, fromIndex) {
-    // write code here
+    if (arguments.length === 0) {
+      return -1;
+    }
+
+    let i = this.length - 1;
+    if (Number.isFinite(fromIndex) && fromIndex > 0
+      && fromIndex < this.length) {
+      i = fromIndex;
+    } else if (Number.isFinite(fromIndex) && fromIndex < 0) {
+      i = this.length + fromIndex;
+    }
+
+    for (i; i >= 0; i--) {
+      if ((!isNaN(searchElement) && searchElement === this[i])
+        || (isNaN(searchElement) && Object.is(searchElement, this[i]))) {
+        return i;
+      }
+    }
+
+    return -1;
   };
 }
 
