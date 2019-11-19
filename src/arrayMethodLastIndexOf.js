946fe4b09344cfb86f4cbdafb05757bbfaacdd5d
Innocent1312 <anthonydev33@gmail.com>
2019-11-19 21:06:16 +0200

    solution fixed

--- src/arrayMethodLastIndexOf.js
+++ src/arrayMethodLastIndexOf.js
@@ -4,13 +4,17 @@
  * Implement method lastIndexOf
  */
 function applyCustomLastIndexOf() {
-  [].__proto__.lastIndexOf2 = function(searchElement, fromIndex = 0) {
-    if (searchElement === undefined) {
+  [].__proto__.lastIndexOf2 = function(searchElement, fromIndex = this.length) {
+    let fromIndexCopy = fromIndex;
+    if (this.length === 0 || searchElement === undefined) {
       return -1;
     }
+    if (fromIndexCopy < 0) {
+      fromIndexCopy += this.length;
+    }
 
-    for (let i = this.length + fromIndex; i >= 0; i--) {
-      if (this[i] === searchElement) {
+    for (let i = fromIndexCopy; i >= 0; i--) {
+      if (searchElement === this[i]) {
         return i;
       }
     }
