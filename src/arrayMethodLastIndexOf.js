135ba553dca3ebeccca431ad9e7dc6fe813b3147
Oles Marola <oles_pw@ukr.net>
2019-11-19 17:08:46 +0200

    last index of

--- src/arrayMethodLastIndexOf.js
+++ src/arrayMethodLastIndexOf.js
@@ -4,12 +4,18 @@
  * Implement method lastIndexOf
  */
 function applyCustomLastIndexOf() {
-  [].__proto__.lastIndexOf2 = function(searchElement, fromIndex = 0) {
+  [].__proto__.lastIndexOf2 = function(searchElement, fromIndex = this.length) {
+    let from = fromIndex;
     if (this.length === 0 || isUndefined(searchElement)) {
       return -1;
     }
-
-    for (let i = this.length + fromIndex; i >= 0; i--) {
+    if (from < 0) {
+      from += this.length;
+    }
+    if (from < 0) {
+      from = this.length;
+    }
+    for (let i = from; i >= 0; i--) {
       if (searchElement === this[i]) {
         return i;
       }
