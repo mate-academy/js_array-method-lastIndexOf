87a8008eb75195ccbd3b698cd1e7807939f78cc7
masha <mariafil2107@gmail.com>
2020-01-26 18:46:45 +0200

    lastIndexOf

--- src/arrayMethodLastIndexOf.js
+++ src/arrayMethodLastIndexOf.js
@@ -4,8 +4,25 @@
  * Implement method lastIndexOf
  */
 function applyCustomLastIndexOf() {
-  [].__proto__.lastIndexOf2 = function(searchElement, fromIndex) {
-    // write code here
+  [].__proto__.lastIndexOf2 = function(searchElement,
+    fromIndex = this.length - 1) {
+    let loopStart = fromIndex;
+
+    if (loopStart < 0) {
+      loopStart = this.length + fromIndex;
+    }
+
+    if (loopStart < 0) {
+      return -1;
+    }
+
+    for (let i = loopStart; i >= 0; i -= 1) {
+      if (searchElement === this[i]) {
+        return i;
+      }
+    }
+
+    return -1;
   };
 }
 
