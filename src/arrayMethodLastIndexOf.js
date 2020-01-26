347444b8953dc59234945da654fccbd45db90195
Viacheslav <slavic.lavrenyuk@gmail.com>
2020-01-26 10:44:22 +0200

    Method lastIndexOf

--- src/arrayMethodLastIndexOf.js
+++ src/arrayMethodLastIndexOf.js
@@ -5,7 +5,29 @@
  */
 function applyCustomLastIndexOf() {
   [].__proto__.lastIndexOf2 = function(searchElement, fromIndex) {
-    // write code here
+    let startIndex = fromIndex || this.length - 1;
+
+    if (startIndex >= this.length) {
+      startIndex = this.length - 1;
+    }
+
+    if (startIndex >= 0) {
+      for (let i = startIndex; i >= 0; i--) {
+        if (this[i] === searchElement) {
+          return i;
+        }
+      }
+    }
+
+    if (startIndex < 0) {
+      for (let i = this.length + startIndex; i >= 0; i--) {
+        if (this[i] === searchElement) {
+          return i;
+        }
+      }
+    }
+
+    return -1;
   };
 }
 
