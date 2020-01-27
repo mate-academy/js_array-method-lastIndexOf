19b73ac6498f49951406c67f545db52bd6cb3bf3
masha <mariafil2107@gmail.com>
2020-01-27 17:44:04 +0200

    renamed var start

--- src/arrayMethodLastIndexOf.js
+++ src/arrayMethodLastIndexOf.js
@@ -6,17 +6,17 @@
 function applyCustomLastIndexOf() {
   [].__proto__.lastIndexOf2 = function(searchElement,
     fromIndex = this.length - 1) {
-    let loopStart = fromIndex;
+    let start = fromIndex;
 
-    if (loopStart < 0) {
-      loopStart = this.length + fromIndex;
+    if (start < 0) {
+      start = this.length + fromIndex;
     }
 
-    if (loopStart < 0) {
+    if (start < 0) {
       return -1;
     }
 
-    for (let i = loopStart; i >= 0; i -= 1) {
+    for (let i = start; i >= 0; i -= 1) {
       if (searchElement === this[i]) {
         return i;
       }
