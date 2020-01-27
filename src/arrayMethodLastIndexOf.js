147d99e2f541cb1f97319d7600ecc4d93446234a
Arimazz <bodyaprymyak@icloud.com>
2020-01-27 20:37:10 +0200

    beta

--- src/arrayMethodLastIndexOf.js
+++ src/arrayMethodLastIndexOf.js
@@ -4,9 +4,34 @@
  * Implement method lastIndexOf
  */
 function applyCustomLastIndexOf() {
-  [].__proto__.lastIndexOf2 = function(searchElement, fromIndex) {
-    // write code here
+  [].__proto__.lastIndexOf2 = function(searchElement, fromIndex = 0) {
+    if (arguments.length === 0) {
+      return -1;
+    }
+
+    let validFromIndex;
+
+    switch (fromIndex) {
+      case 0:
+        validFromIndex = this.length - 1;
+        break;
+      case fromIndex > 0:
+        validFromIndex = fromIndex;
+        break;
+      case fromIndex < 0:
+        validFromIndex = this.length - 1 + fromIndex;
+        break;
+    }
+
+    for (let i = validFromIndex; i >= 0; i--) {
+      if (this[i] === searchElement
+        || (isNaN(this[i]) && isNaN(searchElement) && this[i] !== undefined)) {
+        return i;
+      }
+    }
+
+    return -1;
   };
-}
+};
 
 module.exports = applyCustomLastIndexOf;
