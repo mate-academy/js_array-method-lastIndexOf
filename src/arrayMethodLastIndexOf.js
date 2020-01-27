f1f8851741253d5068ddd2f01a2c76422b95d6d5
Actireon <eheieiei37@gmail.com>
2020-01-27 13:15:35 +0200

    solution

--- src/arrayMethodLastIndexOf.js
+++ src/arrayMethodLastIndexOf.js
@@ -5,8 +5,30 @@
  */
 function applyCustomLastIndexOf() {
   [].__proto__.lastIndexOf2 = function(searchElement, fromIndex) {
-    // write code here
+    if (arguments.length === 0) {
+      return -1;
+    }
+
+    let i;
+
+    if (Math.abs(fromIndex) < this.length) {
+      i = this.length - Math.abs(fromIndex);
+    } else {
+      i = this.length;
+    }
+
+    for (i; i >= 0; i--) {
+      if (Number.isNaN(searchElement)) {
+        return this.findIndex(Number.isNaN);
+      }
+
+      if (this[i] === searchElement) {
+        return i;
+      }
+    }
+
+    return -1;
   };
-}
+};
 
 module.exports = applyCustomLastIndexOf;
