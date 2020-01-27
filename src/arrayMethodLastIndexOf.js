909d8cd359fbe8f98d37d5facdecbe962f302c9d
Senya Volkov <formobileprima@gmail.com>
2020-01-27 12:56:20 +0200

    solution

--- src/arrayMethodLastIndexOf.js
+++ src/arrayMethodLastIndexOf.js
@@ -5,7 +5,28 @@
  */
 function applyCustomLastIndexOf() {
   [].__proto__.lastIndexOf2 = function(searchElement, fromIndex) {
-    // write code here
+    let index;
+    const mainArr = this;
+    const emptyArg = [...arguments];
+
+    if (emptyArg.length === 0) {
+      return -1;
+    }
+
+    if (fromIndex === Math.abs(fromIndex)) {
+      index = fromIndex || mainArr.length;
+    } else {
+      index = Math.abs(fromIndex) < mainArr.length ? mainArr.length
+        - Math.abs(fromIndex) : mainArr.length;
+    }
+
+    for (let i = index; i >= 0; i--) {
+      if (Object.is(mainArr[i], searchElement)) {
+        return i;
+      }
+    }
+
+    return -1;
   };
 }
 
