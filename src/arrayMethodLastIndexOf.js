fe05b572bb3ad2c8a764fc077496d82b97903632
Bohdan Salenik <bohdan.salenik@gmail.com>
2020-01-27 17:19:55 +0200

    so;ution

--- src/arrayMethodLastIndexOf.js
+++ src/arrayMethodLastIndexOf.js
@@ -4,8 +4,25 @@
  * Implement method lastIndexOf
  */
 function applyCustomLastIndexOf() {
-  [].__proto__.lastIndexOf2 = function(searchElement, fromIndex) {
-    // write code here
+  [].__proto__.lastIndexOf2 = function(searchElement, fromIndex = this.length) {
+    if (!arguments.length) {
+      return -1;
+    }
+
+    let currentIndex = (fromIndex < 0) ? this.length + (fromIndex) : fromIndex;
+
+    if (this.length + (fromIndex) < 0) {
+      currentIndex = 0;
+    }
+
+    for (let i = currentIndex; i >= 0; i--) {
+      if ((Number.isNaN(searchElement) && Number.isNaN(this[i]))
+      || this[i] === searchElement) {
+        return i;
+      }
+    }
+
+    return -1;
   };
 }
 
