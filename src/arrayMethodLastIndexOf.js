0cc8f0b28eb3113c726b994d584eb6fb9155ca16
Yaroslav Magrelo <slava.magrelo@gmail.com>
2020-01-28 16:37:25 +0200

    add default to fromIndex, delete extra loop

--- src/arrayMethodLastIndexOf.js
+++ src/arrayMethodLastIndexOf.js
@@ -4,22 +4,23 @@
  * Implement method lastIndexOf
  */
 function applyCustomLastIndexOf() {
-  [].__proto__.lastIndexOf2 = function(searchElement, fromIndex) {
-    if (arguments.length === 0) {
+  [].__proto__.lastIndexOf2 = function(searchElement,
+    fromIndex = this.length - 1) {
+    if (!arguments.length) {
       return -1;
     }
 
-    if (fromIndex < 0) {
-      const negIndex = this.length + fromIndex;
+    let searchFrom = fromIndex;
 
-      for (let i = negIndex; i >= 0; i--) {
-        if (this[i] === searchElement) {
-          return i;
-        }
-      }
+    if (fromIndex < 0) {
+      searchFrom = this.length + fromIndex;
     }
 
-    for (let i = this.length; i > 0; i--) {
+    if (fromIndex >= this.length) {
+      searchFrom = this.length - 1;
+    }
+
+    for (let i = searchFrom; i >= 0; i--) {
       if (this[i] === searchElement) {
         return i;
       }
