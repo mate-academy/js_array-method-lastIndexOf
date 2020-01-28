473814d67ae67bbb69132287e9aa56da41e03f61
oksanaDaniluk <oksana.daniluk@ukr.net>
2020-01-28 12:26:38 +0200

    solution

--- src/arrayMethodLastIndexOf.js
+++ src/arrayMethodLastIndexOf.js
@@ -4,8 +4,33 @@
  * Implement method lastIndexOf
  */
 function applyCustomLastIndexOf() {
-  [].__proto__.lastIndexOf2 = function(searchElement, fromIndex) {
-    // write code here
+  [].__proto__.lastIndexOf2 = function(searchElement, fromIndex = this.length) {
+    if (arguments.length === 0) {
+      return -1;
+    }
+
+    let searchFrom = fromIndex;
+
+    if (fromIndex >= this.length) {
+      searchFrom = this.length;
+    } else if (fromIndex < 0) {
+      searchFrom = this.length + searchFrom;
+    }
+
+    if (searchFrom < 0) {
+      return -1;
+    }
+
+    for (let index = searchFrom; index >= 0; index--) {
+      if (
+        (Number.isNaN(searchElement) && Number.isNaN(this[index]))
+        || searchElement === this[index]
+      ) {
+        return index;
+      }
+    }
+
+    return -1;
   };
 }
 
