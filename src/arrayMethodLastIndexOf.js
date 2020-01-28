d823affd5771b7ffa8eed65a1614112306a190f2
SahaPylypyuk <sashapulupyk@gmail.com>
2020-01-28 17:20:55 +0200

    Task implemented

--- src/arrayMethodLastIndexOf.js
+++ src/arrayMethodLastIndexOf.js
@@ -4,8 +4,24 @@
  * Implement method lastIndexOf
  */
 function applyCustomLastIndexOf() {
-  [].__proto__.lastIndexOf2 = function(searchElement, fromIndex) {
-    // write code here
+  [].__proto__.lastIndexOf2 = function(searchElement, fromIndex = 0) {
+    if (!arguments.length) {
+      return -1;
+    }
+
+    let searchIndex = fromIndex;
+
+    if (searchIndex <= 0) {
+      searchIndex += this.length;
+    }
+
+    for (let i = searchIndex; i >= 0; i--) {
+      if (Object.is(this[i], searchElement)) {
+        return i;
+      }
+    }
+
+    return -1;
   };
 }
 
