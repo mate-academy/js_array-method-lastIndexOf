7391adfc29af8979196975bb46d188c342c6283c
lutfie <serverieva.lutfie@gmail.com>
2020-01-28 13:21:30 +0200

    added method lastIndexOf

--- src/arrayMethodLastIndexOf.js
+++ src/arrayMethodLastIndexOf.js
@@ -4,8 +4,19 @@
  * Implement method lastIndexOf
  */
 function applyCustomLastIndexOf() {
-  [].__proto__.lastIndexOf2 = function(searchElement, fromIndex) {
-    // write code here
+  [].__proto__.lastIndexOf2 = function lastIndexOf2(searchElement,
+    fromIndex = 0) {
+    if (fromIndex > this.length || arguments.length === 0) {
+      return -1;
+    }
+
+    for (let index = this.length + fromIndex; index >= 0; index--) {
+      if (searchElement === this[index]) {
+        return index;
+      }
+    }
+
+    return -1;
   };
 }
 
