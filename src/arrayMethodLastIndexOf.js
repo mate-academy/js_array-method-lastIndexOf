b3ad205ca7870a7f7405e2fe4abf0ea77d3b6f23
Al145 <47367383+Al145@users.noreply.github.com>
2020-01-29 09:04:25 +0200

    solut

--- src/arrayMethodLastIndexOf.js
+++ src/arrayMethodLastIndexOf.js
@@ -4,8 +4,30 @@
  * Implement method lastIndexOf
  */
 function applyCustomLastIndexOf() {
-  [].__proto__.lastIndexOf2 = function(searchElement, fromIndex) {
+  [].__proto__.lastIndexOf2 = function(searchElement, fromIndex =
+  this.length - 1) {
     // write code here
+    if (arguments.length === 0 || fromIndex + this.length < 0) {
+      return -1;
+    }
+
+    let searchFrom = fromIndex;
+
+    if (fromIndex < 0) {
+      searchFrom = this.length + fromIndex;
+    }
+
+    if (searchFrom > this.length - 1) {
+      searchFrom = this.length - 1;
+    }
+
+    for (let i = searchFrom; i >= 0; i--) {
+      if (Object.is(this[i], searchElement)) {
+        return i;
+      }
+    }
+
+    return -1;
   };
 }
 
