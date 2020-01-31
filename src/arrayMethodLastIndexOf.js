677ce89c806eb1a4abc563769c60018f2497dd0b
ded-git <sakarailko@gmail.com>
2020-01-31 14:11:22 +0200

    fix argument's spaces

--- src/arrayMethodLastIndexOf.js
+++ src/arrayMethodLastIndexOf.js
@@ -5,7 +5,9 @@
  */
 function applyCustomLastIndexOf() {
   [].__proto__.lastIndexOf2
-   = function(searchElement, fromIndex = this.length - 1) {
+   = function(
+      searchElement,
+      fromIndex = this.length - 1) {
       let searchFrom = fromIndex;
 
       if (searchFrom >= 0) {
