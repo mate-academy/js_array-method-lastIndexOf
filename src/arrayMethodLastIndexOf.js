e069f6c2f8df63fbe37a3f38686203eacea23730
ded-git <sakarailko@gmail.com>
2020-01-31 18:42:38 +0200

    fix

--- src/arrayMethodLastIndexOf.js
+++ src/arrayMethodLastIndexOf.js
@@ -4,28 +4,27 @@
  * Implement method lastIndexOf
  */
 function applyCustomLastIndexOf() {
-  [].__proto__.lastIndexOf2
-   = function(
-      searchElement,
-      fromIndex = this.length - 1) {
-      let searchFrom = fromIndex;
+  [].__proto__.lastIndexOf2 = function(
+    searchElement,
+    fromIndex = this.length - 1) {
+    let searchFrom = fromIndex;
 
-      if (searchFrom >= 0) {
-        searchFrom = this.length - 1;
+    if (searchFrom >= 0) {
+      searchFrom = this.length - 1;
+    }
+
+    if (searchFrom < 0) {
+      searchFrom = this.length + searchFrom;
+    }
+
+    for (let i = searchFrom; i >= 0; i--) {
+      if (this[i] === searchElement) {
+        return i;
       }
+    }
 
-      if (searchFrom < 0) {
-        searchFrom = this.length + searchFrom;
-      }
-
-      for (let i = searchFrom; i >= 0; i--) {
-        if (this[i] === searchElement) {
-          return i;
-        }
-      }
-
-      return -1;
-    };
+    return -1;
+  };
 }
 
 module.exports = applyCustomLastIndexOf;
