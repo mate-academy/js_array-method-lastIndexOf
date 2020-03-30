42b24da08dafe7b06db6d52dfb71e6cb47d6db3f
Rustam Khananov <antmine2017@gmail.com>
2020-03-30 19:28:37 +0300

    solution

--- src/arrayMethodLastIndexOf.js
+++ src/arrayMethodLastIndexOf.js
@@ -5,8 +5,39 @@
  */
 function applyCustomLastIndexOf() {
   [].__proto__.lastIndexOf2 = function(searchElement, fromIndex) {
-    // write code here
+    let index = fromIndex;
+    const thisLength = this.length;
+
+    if (arguments.length === 0) {
+      return -1;
+    }
+
+    if (fromIndex === undefined
+      || fromIndex === null
+      || Number.isNaN(fromIndex)) {
+      index = thisLength;
+    };
+
+    if (fromIndex < 0) {
+      index = thisLength + fromIndex;
+    };
+
+    if (fromIndex < 0 && -fromIndex > thisLength) {
+      index = thisLength;
+    };
+
+    for (let i = index; i >= 0; i--) {
+      if (this[i] === searchElement) {
+        return i;
+      };
+
+      if (Number.isNaN(searchElement) && Number.isNaN(this[i])) {
+        return i;
+      };
+    };
+
+    return -1;
   };
-}
+};
 
 module.exports = applyCustomLastIndexOf;
