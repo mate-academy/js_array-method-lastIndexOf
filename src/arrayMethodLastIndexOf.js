623a0e18ae78802de8358f2240a4a325abcb1b21
crash2bash <sachagurov@gmail.com>
2020-04-01 12:16:54 +0300

    solution

--- src/arrayMethodLastIndexOf.js
+++ src/arrayMethodLastIndexOf.js
@@ -5,7 +5,24 @@
  */
 function applyCustomLastIndexOf() {
   [].__proto__.lastIndexOf2 = function(searchElement, fromIndex) {
-    // write code here
+    let index = fromIndex;
+    let result = -1;
+
+    if (index < 0) {
+      index += this.length;
+    } else {
+      index = this.length - 1;
+    }
+
+    for (let i = index; i >= 0; i--) {
+      if (this[i] === searchElement) {
+        result = i;
+
+        return result;
+      }
+    }
+
+    return result;
   };
 }
 
