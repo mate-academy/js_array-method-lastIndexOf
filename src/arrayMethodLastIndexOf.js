186ab89cc917220134411e8664d7f3d5460afe23
Igor Kulish <igormagnetic@gmail.com>
2020-04-02 14:41:42 +0300

    sol

--- src/arrayMethodLastIndexOf.js
+++ src/arrayMethodLastIndexOf.js
@@ -4,9 +4,30 @@
  * Implement method lastIndexOf
  */
 function applyCustomLastIndexOf() {
-  [].__proto__.lastIndexOf2 = function(searchElement, fromIndex) {
-    // write code here
-  };
+  [].__proto__.lastIndexOf2
+  = function(searchElement, fromIndex = this.length) {
+      let index = fromIndex;
+
+      if (index < 0) {
+        index += this.length;
+      }
+
+      if (index < 0) {
+        index = this.length;
+      }
+
+      if (this.length === 0 || searchElement === undefined) {
+        return -1;
+      }
+
+      for (let i = index; i >= 0; i--) {
+        if (Object.is(this[i], searchElement)) {
+          return i;
+        }
+      };
+
+      return -1;
+    };
 }
 
 module.exports = applyCustomLastIndexOf;
