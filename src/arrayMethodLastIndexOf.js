14855c477a9ef83b62bad162d72ce30392fd2f31
Andrii Badekha <badekha@gmail.com>
2020-04-02 22:22:52 +0300

    solution 1

--- src/arrayMethodLastIndexOf.js
+++ src/arrayMethodLastIndexOf.js
@@ -5,7 +5,35 @@
  */
 function applyCustomLastIndexOf() {
   [].__proto__.lastIndexOf2 = function(searchElement, fromIndex) {
-    // write code here
+    let startFind = fromIndex;
+
+    if (!startFind) {
+      startFind = this.length;
+    }
+
+    if (arguments.length === 0) {
+      return -1;
+    }
+
+    if (fromIndex < 0) {
+      startFind = this.length + fromIndex;
+    }
+
+    if (startFind < 0) {
+      startFind = this.length;
+    }
+
+    for (let i = startFind; i >= 0; i--) {
+      if (this[i] === searchElement) {
+        return i;
+      }
+
+      if (Number.isNaN(searchElement) && Number.isNaN(this[i])) {
+        return i;
+      }
+    }
+
+    return -1;
   };
 }
 
