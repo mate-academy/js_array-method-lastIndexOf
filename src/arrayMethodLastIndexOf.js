96a866e645eaa6effcb0315fa4472921515964f0
nvaho <iinazarovc@gmail.com>
2020-04-05 21:35:02 +0300

    fff

--- src/arrayMethodLastIndexOf.js
+++ src/arrayMethodLastIndexOf.js
@@ -4,8 +4,20 @@
  * Implement method lastIndexOf
  */
 function applyCustomLastIndexOf() {
-  [].__proto__.lastIndexOf2 = function(searchElement, fromIndex) {
-    // write code here
+  [].__proto__.lastIndexOf2 = function(searchElement, fromIndex = -1) {
+    let startIndex = fromIndex;
+
+    if (startIndex < 0) {
+      startIndex += this.length;
+    }
+
+    for (let i = startIndex; i >= 0; i--) {
+      if (Object.is(this[i], searchElement)) {
+        return i;
+      }
+    }
+
+    return -1;
   };
 }
 
