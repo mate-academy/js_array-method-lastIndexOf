6288801b6b93d7fddc259fa5b4a40ec98d11deab
Vitaliy <vitaliyanohin@gmail.com>
2020-04-17 13:17:20 +0300

    sol

--- src/arrayMethodLastIndexOf.js
+++ src/arrayMethodLastIndexOf.js
@@ -5,7 +5,34 @@
  */
 function applyCustomLastIndexOf() {
   [].__proto__.lastIndexOf2 = function(searchElement, fromIndex) {
-    // write code here
+    let index = this.length - 1;
+    const elementExist = -1;
+
+    if (fromIndex === null | fromIndex === undefined) {
+      index = this.length - 1;
+    }
+
+    if (fromIndex < 0) {
+      index = this.length + fromIndex;
+    }
+
+    if (index < 0 | (searchElement === undefined & fromIndex === undefined)) {
+      return elementExist;
+    }
+
+    for (index; index >= 0; index--) {
+      if (this[index] !== undefined
+        & isNaN(this[index])
+        & isNaN(searchElement)) {
+        return index;
+      }
+
+      if (this[index] === searchElement) {
+        return index;
+      }
+    }
+
+    return elementExist;
   };
 }
 
