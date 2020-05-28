03ad6c9b83144f91e622e730dab3db64f7ce002d
Denys <gryshko_denys@gmail.com>
2020-05-28 20:39:58 +0300

    solution

--- src/arrayMethodLastIndexOf.js
+++ src/arrayMethodLastIndexOf.js
@@ -4,9 +4,37 @@
  * Implement method lastIndexOf
  */
 function applyCustomLastIndexOf() {
-  [].__proto__.lastIndexOf2 = function(searchElement, fromIndex) {
-    // write code here
+  [].__proto__.lastIndexOf2 = function(searchElement,
+    fromIndex = this.length - 1) {
+    if (searchElement === undefined) {
+      return -1;
+    }
+
+    let newFromIndex = fromIndex;
+
+    if (newFromIndex >= this.length) {
+      return -1;
+    } else if (Math.abs(newFromIndex) > this.length) {
+      newFromIndex = 0;
+    } else if (fromIndex < 0) {
+      newFromIndex = (this.length + fromIndex);
+    }
+
+    for (let i = newFromIndex; i >= 0; i--) {
+      if (Number.isNaN(searchElement) === false) {
+        if (this[i] === searchElement) {
+          return i;
+        }
+      } else if (Number.isNaN(searchElement)) {
+        if (Number.isNaN(this[i])) {
+          return i;
+        }
+      }
+    }
+
+    return -1;
   };
 }
-
+// applyCustomLastIndexOf();
+// console.log(['11', '22', '33', '', 44, '11', 55].lastIndexOf2());
 module.exports = applyCustomLastIndexOf;
