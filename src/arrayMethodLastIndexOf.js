814351988c86e498a8b58507b07c47dd3913618d
DimaDP <dimadp95@gmail.com>
2020-05-28 19:09:08 +0300

    dimadp__solution

--- src/arrayMethodLastIndexOf.js
+++ src/arrayMethodLastIndexOf.js
@@ -4,9 +4,38 @@
  * Implement method lastIndexOf
  */
 function applyCustomLastIndexOf() {
-  [].__proto__.lastIndexOf2 = function(searchElement, fromIndex) {
-    // write code here
-  };
+  [].__proto__.lastIndexOf2
+    = function(searchElement, fromIndex = this.length - 1) {
+      let i = fromIndex;
+
+      if (searchElement === undefined) {
+        return -1;
+      }
+
+      if (fromIndex < 0) {
+        if (this.length + fromIndex < 0) {
+          i = this.length - 1;
+        } else {
+          i = this.length + fromIndex;
+        }
+      } else {
+        i = this.length + fromIndex;
+      }
+
+      for (i; i >= 0; i--) {
+        if (this[i] === searchElement
+          || (this[i] === null && searchElement === null)) {
+          return i;
+        }
+
+        if (Number.isNaN(this[i])
+        && Number.isNaN(searchElement)) {
+          return i;
+        }
+      }
+
+      return -1;
+    };
 }
 
 module.exports = applyCustomLastIndexOf;
