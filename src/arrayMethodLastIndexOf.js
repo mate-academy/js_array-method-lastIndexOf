8730be70c3b7ce03e39e74dec002cd3e962f6b8e
tyooma <flying.alien.godzilla@gmail.com>
2020-05-30 21:42:33 +0300

    DIY lastIndexOf Method

--- src/arrayMethodLastIndexOf.js
+++ src/arrayMethodLastIndexOf.js
@@ -4,8 +4,27 @@
  * Implement method lastIndexOf
  */
 function applyCustomLastIndexOf() {
-  [].__proto__.lastIndexOf2 = function(searchElement, fromIndex) {
-    // write code here
+  // eslint-disable-next-line max-len
+  [].__proto__.lastIndexOf2 = function(searchElement, fromIndex = this.length - 1) {
+    if (fromIndex > this.length || this.length === 0) {
+      return -1;
+    } else if (fromIndex < -this.length) {
+      // eslint-disable-next-line no-param-reassign
+      fromIndex = this.length - 1;
+    }
+
+    if (fromIndex < 0) {
+      // eslint-disable-next-line no-param-reassign
+      fromIndex = this.length + fromIndex;
+    }
+
+    for (let i = fromIndex; i >= 0; i--) {
+      if (Object.is(searchElement, this[i])) {
+        return i;
+      }
+    }
+
+    return -1;
   };
 }
 
