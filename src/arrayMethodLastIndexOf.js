9fdac200f6b3febf086ee1d6146f494821cf5d02
DTR <t.terry.rice@gmail.com>
2020-05-30 18:26:12 +0300

    final 1.0

--- src/arrayMethodLastIndexOf.js
+++ src/arrayMethodLastIndexOf.js
@@ -5,7 +5,21 @@
  */
 function applyCustomLastIndexOf() {
   [].__proto__.lastIndexOf2 = function(searchElement, fromIndex) {
-    // write code here
+    let startIndex = fromIndex;
+
+    if (fromIndex < 0) {
+      startIndex += this.length;
+    } else {
+      startIndex = this.length - 1;
+    }
+
+    for (let i = startIndex; i >= 0; i--) {
+      if (Object.is(searchElement, this[i])) {
+        return i;
+      }
+    };
+
+    return -1;
   };
 }
 
